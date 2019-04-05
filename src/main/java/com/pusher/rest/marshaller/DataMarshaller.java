0118bb9e3e487541ebf9666136da0e4f6e15a252
kn100 <backupa76@gmail.com>
2019-04-05 14:56:38 +0100

    remove author tags - git history gives credit :D

--- src/main/java/com/pusher/rest/marshaller/DataMarshaller.java
+++ src/main/java/com/pusher/rest/marshaller/DataMarshaller.java
@@ -1,8 +1,5 @@
 package com.pusher.rest.marshaller;
 
-/**
- * @author <a href="mailto:marvenec@gmail.com">Martin Večeřa</a>
- */
 public interface DataMarshaller {
 
    String marshal(final Object data);
--- src/main/java/com/pusher/rest/marshaller/DefaultDataMarshaller.java
+++ src/main/java/com/pusher/rest/marshaller/DefaultDataMarshaller.java
@@ -2,9 +2,6 @@ package com.pusher.rest.marshaller;
 
 import com.google.gson.Gson;
 
-/**
- * @author <a href="mailto:marvenec@gmail.com">Martin Večeřa</a>
- */
 public class DefaultDataMarshaller implements DataMarshaller {
 
    private final Gson gson = new Gson();
