3e43680f359e8f81b2b57dfb1ae943d92067f6f8
Christophe Maillard <christophe@qudini.com>
2020-02-04 10:54:52 +0000

    Adding PusherNoHttp for non-http related tests

--- src/test/java/com/pusher/rest/PusherTestUrlConfig.java
+++ src/test/java/com/pusher/rest/PusherTestUrlConfig.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.lang.reflect.Field;
 
+import com.pusher.rest.util.PusherNoHttp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,7 @@ public class PusherTestUrlConfig {
 
     @Test
     public void testUrl() throws Exception {
-        Pusher p = new Pusher("https://key:secret@api.example.com:4433/apps/00001");
+        PusherNoHttp p = new PusherNoHttp("https://key:secret@api.example.com:4433/apps/00001");
 
         assertField(p, "scheme", "https");
         assertField(p, "key", "key");
@@ -23,7 +24,7 @@ public class PusherTestUrlConfig {
 
     @Test
     public void testUrlNoPort() throws Exception {
-        Pusher p = new Pusher("http://key:secret@api.example.com/apps/00001");
+        PusherNoHttp p = new PusherNoHttp("http://key:secret@api.example.com/apps/00001");
 
         assertField(p, "scheme", "http");
         assertField(p, "key", "key");
@@ -35,33 +36,33 @@ public class PusherTestUrlConfig {
     @Test
     public void testUrlMissingField() throws Exception {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Pusher("https://key@api.example.com:4433/apps/appId");
+            new PusherNoHttp("https://key@api.example.com:4433/apps/appId");
         });
     }
 
     @Test
     public void testUrlEmptySecret() throws Exception {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Pusher("https://key:@api.example.com:4433/apps/appId");
+            new PusherNoHttp("https://key:@api.example.com:4433/apps/appId");
         });
     }
 
     @Test
     public void testUrlEmptyKey() throws Exception {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Pusher("https://:secret@api.example.com:4433/apps/appId");
+            new PusherNoHttp("https://:secret@api.example.com:4433/apps/appId");
         });
     }
 
     @Test
     public void testUrlInvalidScheme() throws Exception {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new Pusher("telnet://key:secret@api.example.com:4433/apps/appId");
+            new PusherNoHttp("telnet://key:secret@api.example.com:4433/apps/appId");
         });
     }
 
-    private static <V> void assertField(final Object o, final String fieldName, final V expected) throws Exception {
-        final Field field = o.getClass().getDeclaredField(fieldName);
+    private static <T extends PusherAbstract<?>, V> void assertField(final T o, final String fieldName, final V expected) throws Exception {
+        final Field field = PusherAbstract.class.getDeclaredField(fieldName);
         field.setAccessible(true);
         final V actual = (V)field.get(o);
 
