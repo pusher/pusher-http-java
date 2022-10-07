f62493ebded7a2cc6c9bc6af663a520a85e198e2
sonologico <1592315+sonologico@users.noreply.github.com>
2022-10-07 17:27:40 +0200

    Disable Gson html encoding (#64)

--- src/main/java/com/pusher/rest/marshaller/DefaultDataMarshaller.java
+++ src/main/java/com/pusher/rest/marshaller/DefaultDataMarshaller.java
@@ -1,13 +1,17 @@
 package com.pusher.rest.marshaller;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class DefaultDataMarshaller implements DataMarshaller {
 
    private final Gson gson;
 
    public DefaultDataMarshaller() {
-      gson = new Gson();
+       gson = new GsonBuilder()
+           .disableHtmlEscaping()
+           .create();
    }
 
    public DefaultDataMarshaller(Gson customGsonInstance) {
